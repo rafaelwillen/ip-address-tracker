cfdd294d18f18555dee36458ddb202ae5fccbcac
Rafael Willen <rafaelpadre20@gmail.com>
2022-12-05 16:30:56 +0100

    Style the heading for mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,10 +5,10 @@ const Header = () => {
   return (
     <header className="bg-neutral-400 pt-8 pattern-bg">
       <Container>
-        <h1 className="text-xl text-neutral-300 text-center">
+        <h1 className="text-xl text-neutral-200 font-medium text-center lg:text-2xl">
           IP Address Tracker
         </h1>
-        <form className="mt-8 pb-40 text-center">
+        <form className="mt-8 pb-40 flex justify-center w-full">
           <Input />
         </form>
       </Container>
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -2,7 +2,7 @@ import ArrowSVG from "../assets/icon-arrow.svg";
 
 const Input = () => {
   return (
-    <div className="flex">
+    <div className="flex max-w-2xl flex-1 ">
       <input
         type="text"
         className="w-full py-5 px-6 rounded-l-3xl flex-1 text-neutral-400 text-ellipsis placeholder:text-neutral-300"
