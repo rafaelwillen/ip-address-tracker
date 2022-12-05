ea5d7dffe0d97d30692e3531d67f94ea82162280
Rafael Willen <rafaelpadre20@gmail.com>
2022-12-05 15:48:07 +0100

    Add app theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,23 @@ module.exports = {
     "./src/**/*.{ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        inputSize: "1.25rem"
+      }
+    },
+    colors: {
+      neutral: {
+        100: "#fff",
+        200: "#f4f4f4",
+        300: "#969696",
+        400: "#2b2b2b",
+        500: "#000"
+      }
+    },
+    fontFamily: {
+      main: "'Rubik', sans-serif"
+    }
   },
   plugins: [],
 }
