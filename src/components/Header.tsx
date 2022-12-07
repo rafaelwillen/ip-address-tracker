43449cdab7bce921bc6ba4ce5b2b72772313e2a8
Rafael Willen <rafaelpadre20@gmail.com>
2022-12-07 12:38:05 +0100

    Add state to form

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,27 @@
+import { FormEvent, useContext } from "react";
+import AppContext from "../context/AppContext";
 import Container from "./Container";
 import Input from "./Input";
 
 const Header = () => {
+  const { ipAddress, setIpAddress } = useContext(AppContext);
+
+  const onSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    console.log(ipAddress);
+  };
+
   return (
     <header className="bg-neutral-400 pt-8 pattern-bg">
       <Container>
         <h1 className="text-xl text-neutral-200 font-medium text-center lg:text-2xl">
           IP Address Tracker
         </h1>
-        <form className="mt-8 pb-40 flex justify-center w-full">
-          <Input />
+        <form
+          onSubmit={onSubmit}
+          className="mt-8 pb-40 flex justify-center w-full"
+        >
+          <Input value={ipAddress} onChange={setIpAddress} />
         </form>
       </Container>
     </header>
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,9 +1,17 @@
+import { FC } from "react";
 import ArrowSVG from "../assets/icon-arrow.svg";
 
-const Input = () => {
+type Props = {
+  value: string;
+  onChange: (value: string) => void;
+};
+
+const Input: FC<Props> = ({ onChange, value }) => {
   return (
     <div className="flex max-w-2xl flex-1 ">
       <input
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
         type="text"
         className="w-full py-5 px-6 rounded-l-3xl flex-1 text-neutral-400 text-ellipsis placeholder:text-neutral-300 outline-none"
         placeholder="Search for any IP address"
